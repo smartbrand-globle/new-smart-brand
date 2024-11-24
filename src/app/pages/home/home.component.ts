6e782d221c550b4be3e80e569ea4e573b3d05a2e
Amir Sami Fekri <amir.sami.fekri@gmail.com>
2024-11-24 18:42:11 +0200

    make nav bar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderHomeComponent } from './components/header-home/header-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderHomeComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
