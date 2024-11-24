eb49bf9b75fc5439a6d0c4d76c5747d519149245
ahmedhussein70 <ahmed.hussein201170@gmail.com>
2024-11-24 16:42:30 +0200

    make routering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesesComponent } from './pages/serviceses/serviceses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesesComponent },
+  { path: 'web-development', component: HomeComponent },
+  { path: 'mobile-app', component: HomeComponent },
+  { path: 'ui-ux-design', component: HomeComponent },
+  { path: 'sco', component: HomeComponent },
+];
