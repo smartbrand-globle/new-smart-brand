d7b72257fdc553f945a13ece8241ee9294b963b0
Amir Sami Fekri <amir.sami.fekri@gmail.com>
2024-11-25 14:29:03 +0200

    the first three sections in About

---
+++ src/app/pages/about/components/what-do-about/what-do-about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-what-do-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './what-do-about.component.html',
+  styleUrl: './what-do-about.component.css'
+})
+export class WhatDoAboutComponent {
+
+}
---
+++ src/app/pages/about/components/who-are-about/who-are-about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-are-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './who-are-about.component.html',
+  styleUrl: './who-are-about.component.css'
+})
+export class WhoAreAboutComponent {
+
+}
