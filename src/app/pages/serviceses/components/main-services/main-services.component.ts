58604f20fb6c1d8c2d48883256a7d24a61c36fc8
ahmedhussein70 <ahmed.hussein201170@gmail.com>
2024-11-28 17:13:42 +0200

    fix responsive and make router link

--- src/app/pages/serviceses/components/main-services/main-services.component.ts
+++ src/app/pages/serviceses/components/main-services/main-services.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-services',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-services.component.html',
-  styleUrl: './main-services.component.css'
+  styleUrl: './main-services.component.css',
 })
-export class MainServicesComponent {
-
-}
+export class MainServicesComponent {}
