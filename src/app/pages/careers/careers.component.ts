0eec67686bcd751f23753aa22e5891e9c2f19ddc
ahmedhussein70 <ahmed.hussein201170@gmail.com>
2024-11-30 10:45:10 +0200

    fix about reponsive

--- src/app/pages/careers/careers.component.ts
+++ src/app/pages/careers/careers.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormCareersComponent } from "./form-careers/form-careers.component";
 
 @Component({
   selector: 'app-careers',
   standalone: true,
-  imports: [],
+  imports: [FormCareersComponent],
   templateUrl: './careers.component.html',
   styleUrl: './careers.component.css'
 })
---
+++ src/app/pages/careers/form-careers/form-careers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-careers',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-careers.component.html',
+  styleUrl: './form-careers.component.css'
+})
+export class FormCareersComponent {
+
+}
